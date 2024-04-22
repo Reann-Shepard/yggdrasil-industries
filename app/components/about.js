75467323983aec90bfba174e67be857ba166cd28
Linden <tykor.wulfen@gmail.com>
2024-04-22 14:20:17 -0600

    Update about.js
    Fixed issue React presented with '

--- app/components/about.js
+++ app/components/about.js
@@ -10,13 +10,13 @@ const About = () => {
           As a published writer, Linden Wright knows how to navigate deadlines
           and client expectations, while still tactfully accepting feedback and
           last minute changes. Always amenable to teamwork and extra-curricular
-          learning, Linden is fluent in 5 languages, can integrate into any size
+          learning, they are fluent in 5 languages, can integrate into any size
           group and make useful contributions while still keeping his own voice.
-          Linden's goal is to gain relevant work experience as a software
-          developer and work towards his Canadian permanent residency.
+          Their goal is to gain relevant work experience as a software developer
+          and work towards his Canadian permanent residency.
           <br />
           <br />
-          Be sure to check out his projects and contact him if you have any
+          Be sure to check out their projects and contact them if you have any
           questions or opportunities.
         </p>
         <div className="w-1/4"></div>
